506edda8db7f4c0e0872ea0e8318d00c4b93a028
akindziora <kindziora@live.de>
2019-12-26 16:02:09 +0100

    added more

---
+++ src/dom/list.ts
@@ -0,0 +1,37 @@
+import {kelement} from "./element.js";
+
+export class list {
+
+    public id: String;
+    public $el: HTMLElement;
+    public $scope: HTMLElement;
+
+    constructor(el: HTMLElement, $scope: HTMLElement, counter:number = 1) {
+        this.$el = el;
+        this.$scope = $scope;
+        this.setId(this.$scope.getAttribute('data-id') || undefined, counter);
+    }
+ 
+    public getName(){
+        return this.$el.getAttribute('data-name');
+    }
+
+    setId(namesp: string = "", counter:number) {
+        let id: string = namesp+ "-" + counter;
+        this.$el.setAttribute("data-id", id);
+        this.id = id;
+    }
+
+    public addElement(element : kelement ){
+
+    }
+
+    public removeElement(){
+
+    }
+
+    public replaceElement(){
+        
+    }
+    
+}
