4efad57ee3e2335cc8cda8358f15e74b649869f7
akindziora <kindziora@live.de>
2020-01-13 09:47:16 +0100

    added more

--- build/app.js
+++ build/app.js
@@ -1,8 +1,34 @@
+import { component } from "./component.js";
+import { dom } from './dom.js';
+import { eventHandler } from './eventHandler.js';
+import { dataHandler } from './dataHandler.js';
 export class app {
     constructor() {
+        this.components = {};
+        this.dataH = new dataHandler(new eventHandler());
     }
-    createComponent(name, html, store, actions) {
+    /**
+     *
+     * @param name
+     * @param html
+     * @param data
+     * @param actions
+     * @param injections
+     */
+    createComponent(name, html, data, actions, injections) {
+        let store = this.dataH.createStore(name, data);
+        let el = document.createElement("div");
+        el.outerHTML = html;
+        el.setAttribute("data-name", name);
+        this.components[name] = new component(new dom(el, injections), store, actions);
+        return this.components[name];
     }
     removeComponent() {
+    }
+    /**
+     *
+     * @param url
+     */
+    render(url) {
     }
 }
--- src/app.ts
+++ src/app.ts
@@ -4,16 +4,47 @@ import { eventHandler } from './eventHandler.js';
 import { dataHandler } from './dataHandler.js';
 
 export class app {
+    public dataH: dataHandler;
+    public components: { [index: string]: component } = {};
 
-    constructor(){
+    constructor() {
+        this.dataH = new dataHandler(new eventHandler());
 
     }
+    /**
+     * 
+     * @param name 
+     * @param html 
+     * @param data 
+     * @param actions 
+     * @param injections 
+     */
+    createComponent(name: string, html: string, data: object, actions: object, injections: object) {
 
-    createComponent(name, html, store, actions) {
+        let store = this.dataH.createStore(name, data);
+        let el = document.createElement("div");
+        el.outerHTML = html;
+        el.setAttribute("data-name", name);
 
+        this.components[name] = new component(new dom(el, injections), store, actions);
+
+        return this.components[name];
     }
-  
+
     removeComponent() {
-  
+
     }
+
+    /**
+     * 
+     * @param url 
+     */
+    render(url:string){
+
+    }
+
+
+
+
+
 }
