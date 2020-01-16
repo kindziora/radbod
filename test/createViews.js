7a22d10967df4c9e0e5b104c2114e3674b3a10c6
kindziora@live.de <kindziora@live.de>
2020-01-16 15:21:32 +0100

    middleware events

--- test/createViews.js
+++ test/createViews.js
@@ -2,15 +2,18 @@ import {dom} from '../build/dom.js';
 import browserEnv from 'browser-env';
 browserEnv();
 
-let el = document.createElement("div"); 
-el.innerHTML = '<form method="post" ><h1 data-name="/$user/name">Payment form <b>${user.name}</b></h1><p>Required fields are followed by <strong><abbr title="required">*</abbr></strong>.</p>${user}</form>';
+let el = document.createElement("div");
+el.innerHTML = '<form method="post" ><h1 data-name="/$user/name">Payment form <b>${data.value.name}</b></h1><p>Required fields are followed by <strong><abbr title="required">*</abbr></strong>.</p>${data.value}</form>';
 
-
-let els = Array.from(el.querySelectorAll("[data-name]"));
+let d = new dom(el);
 
 let views = {};
-
-for(let e of els){
-  e.setAttribute("data-view", "viewxyz");
-  views["viewxyz"] = e.innerHTML;
+for(let i in d.element){
+  let e = d.element[i];
+  if(!e.$el.hasAttribute("data-view")){
+    e.$el.setAttribute("data-view", e.id);
+    e.setTemplate(eval('(data)=>`'+ e.$el.innerHTML +'`'));
+    console.log(e.template({value: {name: 234234}}));
+  }
+  
 }
