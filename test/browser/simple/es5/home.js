daa9d31df74c7b3f143aa3ea4571ca9b3cad532e
akindziora <kindziora@live.de>
2020-01-17 22:41:07 +0100

    added more

--- test/browser/simple/es5/home.js
+++ test/browser/simple/es5/home.js
@@ -1,24 +1,28 @@
 
 let tplELEMENT = document.getElementsByTagName("home")[0];
 
+let userStore = app.dataH.createStore("user", {
+    "name": "alexander kindziora",
+    "info": {
+        gender: "male",
+        "age" : 32
+    } 
+});
+
+let interactions = {
+    "/$user/name": {
+        "click"(sender, dataStore) { //address specific element in dom
+            console.log('CLICK', sender.field.getValue());
+            dataStore.name += " ,";
+          
+        }
+    }
+};
+
 app.createComponent(
     "home", {home : tplELEMENT.innerHTML},
-    app.dataH.createStore("user", {
-        "name": "alexander kindziora",
-        "info": {
-            gender: "male",
-            "age" : 32
-        } 
-    }),
-    {
-        "/$user/name": {
-            "click"(sender, dataStore) { //address specific element in dom
-                console.log('CLICK', sender.field.getValue());
-                dataStore.name += " ,";
-              
-            }
-        }
-    }, {
+    userStore,
+    interactions, {
     card : app.components.card
 });
 
