546adc535d09f90dfa0953f44729a974ed44ecd9
kindziora@live.de <kindziora@live.de>
2020-01-20 09:08:11 +0100

    middleware events

--- test/serve.js
+++ test/serve.js
@@ -1,5 +1,7 @@
 // import express (after npm install express)
 import express from 'express';
+import path from 'path';
+const __dirname = path.resolve();
 
 // create new express app and save it as "app"
 const app = express();
@@ -7,8 +9,10 @@ const app = express();
 // server configuration
 const PORT = 8800;
 
-app.use(express.static('/home/akindziora/projekte/kjs/test/browser'));
+app.use(express.static(path.join(__dirname, 'browser')));
  
+console.log(path.join(__dirname, 'test/browser'));
+
 app.use(function (err, req, res, next) {
   console.log(req);
   return res.status(500).send({ error: err });
