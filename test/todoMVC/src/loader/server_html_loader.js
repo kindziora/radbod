a06e4e893a01455e6bf791f2bae2d64a3eb204f2
kindziora@live.de <kindziora@live.de>
2020-02-04 18:26:46 +0100

    added new stuff

--- test/todoMVC/src/loader/server_html_loader.js
+++ test/todoMVC/src/loader/server_html_loader.js
@@ -1,19 +1,36 @@
+import { eventHandler } from './eventHandler.js';
+import { dataHandler } from './dataHandler.js';
 
 import path from 'path';
 const __dirname = path.resolve();
-
+const base = "/home/akindziora/Downloads/projekte/radbod/test/todoMVC/public/build/dev/";
 
 export function html_loader(req, res, next){
     
-    const srcPath = __dirname + "/test/browser/app";
-    const page = "/page";
-    const view = "/view";
-    const code = "/code";
+    let page = import(base + "page/home.js").home;
+  
+    let dataH = new dataHandler(new eventHandler(), environment);
 
-    console.log(__dirname, req.url);
+    //recursive fetching of component data
+    
+    fetchData(page, function(data){
+        let renderedHTML = page.views.home.apply(null, this.stores.toArray());
 
+        console.log(data, renderedHTML);
+    
+        next(); 
+        
+    });
     
 
-    next();
+    function fetchData(component, callback) {
+        component.data.call(dataH, callback);
+        for(let i in component.components){
+            fetchData(component.components[i])
+        }
+    }
+
+   
+
 
 }
