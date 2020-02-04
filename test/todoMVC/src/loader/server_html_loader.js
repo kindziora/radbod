be3a63b1ceb2f63fdd25ce906902fc3ad5fba7ba
kindziora@live.de <kindziora@live.de>
2020-02-04 18:31:09 +0100

    added new stuff

--- test/todoMVC/src/loader/server_html_loader.js
+++ test/todoMVC/src/loader/server_html_loader.js
@@ -12,22 +12,34 @@ export function html_loader(req, res, next){
     let dataH = new dataHandler(new eventHandler(), environment);
 
     //recursive fetching of component data
-    
-    fetchData(page, function(data){
-        let renderedHTML = page.views.home.apply(null, this.stores.toArray());
 
-        console.log(data, renderedHTML);
-    
-        next(); 
+    fetchData(page, function(data){
+
+        if(fetchedAll){
+            let renderedHTML = page.views.home.apply(null, this.stores.toArray());
+
+            console.log(data, renderedHTML);
+        
+            next(); 
+        }
+     
         
     });
     
-
-    function fetchData(component, callback) {
+    /**
+     * 
+     * @param {*} component 
+     * @param {*} callback 
+     * @param {*} allready 
+     */
+    function fetchData(component, callback, allready) {
         component.data.call(dataH, callback);
         for(let i in component.components){
             fetchData(component.components[i])
         }
+       if(Object.keys(component.components).length === 0){
+            allready(dataH);
+       }
     }
 
    
