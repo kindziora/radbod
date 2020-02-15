7e17d5d7e5b676c1260255f85bbf42a3ef3f4adf
akindziora <kindziora@live.de>
2020-02-15 15:40:52 +0100

    load all data

---
+++ server/copyFiles.js
@@ -0,0 +1,15 @@
+import { getFiles } from './files.js';
+import { promises as fs } from 'fs';
+
+export async function copyFiles(folder, options) {
+    for await (const file of getFiles(folder || '/home/akindziora/projekte/kjs/test/todoMVC/src/')) {
+        let folderPath = file.replace("src", options.buildPath || "public/build/dev").split("/");
+        let filename = folderPath.pop();
+
+        if (filename.split(".")[1] === options.extension) {
+            await fs.mkdir(folderPath.join("/"), { recursive: true });
+            await fs.copyFile(file, file.replace("src", options.buildPath || "public/build/dev"));
+        }
+        
+    }
+}
---
+++ test/todoMVC/public/build/dev/loader/server_html_loader.js
@@ -0,0 +1,93 @@
+import { dataHandler } from '../../../../build/dataHandler.js';
+import { eventHandler } from '../../../../build/eventHandler.js';
+
+import path from 'path';
+import { getFile } from '../../config/routes.js';
+
+const __dirname = path.resolve();
+const base = "/home/akindziora/projekte/radbod/test/todoMVC/public/build/dev/";
+
+const asyncHandler = fn => (req, res, next) =>
+    Promise
+        .resolve(fn(req, res, next))
+        .catch(next)
+
+const enviroment = {
+    data_loader: {
+        find(options, cb) {
+            setTimeout(() => cb.call({ dataH: {} }, { name: "test load asynchronous" }), 0);
+        }
+    }
+};
+
+export const html_loader = asyncHandler(async function (req, res, next) {
+
+    function fetchData(component, cb, allready, total, meta) {
+
+        let callback = function (meta) {
+            return (data) => {
+                cb(data);
+                meta.cnt++;
+                if (meta.cnt >= total) {
+                    allready(dataH);
+                }
+            }
+        };
+
+        let result = component.data.call(dataH, callback(meta), {});
+
+        if (typeof result.then !== "function") {
+            meta.cnt++;
+        }
+
+        for (let i in component.components) {
+            fetchData(component.components[i], cb, allready, total, meta);
+        }
+
+        if (meta.cnt >= total) {
+            allready(dataH);
+        }
+    }
+
+    function countForData(component, cnt) {
+        for (let i in component.components)
+            cnt = countForData(component.components[i], cnt);
+        return ++cnt;
+    }
+
+    let dataH = new dataHandler(new eventHandler(), enviroment);
+
+    let path = req.path;
+    
+    let f = getFile(path);
+
+    console.log(path, f);
+
+    let page = await import(base + "page/" + f + ".js");
+
+    let count = countForData(page[f], 0);
+    let met = { cnt: 0 };
+
+    fetchData(page[f], (data) => {
+    }, (stores) => {
+        let renderedHTML = '';
+
+        let storeData = stores.store.toArray();
+
+        console.log(storeData);
+
+        try {
+            renderedHTML = page[f].views[f].apply(null, [{ value: "" }, ...storeData]);
+        } catch (e) {
+            console.log(renderedHTML, e);
+        }
+
+        renderedHTML
+
+        res.send(renderedHTML);
+
+        next();
+
+    }, count, met);
+
+});
